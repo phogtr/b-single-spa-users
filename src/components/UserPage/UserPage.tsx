0bda03720a7331108eee024475e37e49302eba4a
phogtr <phogbtr@gmail.com>
2021-11-14 12:32:12 -0800

    added styled

--- src/components/UserPage/UserPage.tsx
+++ src/components/UserPage/UserPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Table } from "../Table/Table";
+import { Button, Checkbox } from "antd";
 
 export const UserPage: React.FC<{}> = () => {
   return (
     <section>
       <h1>Users</h1>
+      <Button type="ghost">Test Button</Button>
+      <Checkbox>Test Checkbox</Checkbox>
       <Table />
     </section>
   );
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import { UserPage } from "./components/UserPage/UserPage";
 
+import "antd/dist/antd.css";
+
 export default function Root() {
   return (
     <BrowserRouter>
