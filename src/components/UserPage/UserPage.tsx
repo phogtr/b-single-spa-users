df26bc189ad1eed88da46a1762f3cce07189a189
phogtr <phogbtr@gmail.com>
2021-11-16 20:42:23 -0800

    used shared-component

--- src/components/UserPage/UserPage.tsx
+++ src/components/UserPage/UserPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Table } from "../Table/Table";
-import { Button, Checkbox } from "antd";
+
+//@ts-ignore
+import { Button } from "@react-mf/shared-components";
 
 export const UserPage: React.FC<{}> = () => {
   return (
     <section>
       <h1>Users</h1>
-      <Button type="ghost">Test Button</Button>
-      <Checkbox>Test Checkbox</Checkbox>
+      <Button text={"echo hello world"} />
       <Table />
     </section>
   );
